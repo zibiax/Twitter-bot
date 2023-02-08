7d1c371b58d105a4ce58f5e5c98482226ce4fdb9
Martin <martin.evenbom@gmail.com>
2023-02-08 11:40:06 +0100

    Logic for mediatweet added

--- Program.cs
+++ Program.cs
@@ -8,31 +8,60 @@ using TweetSharp;
 
 namespace TwitterBot {
     class Program {
-        private static string customer_key = "";
-        private static string customer_key_secret = "";
-        private static string access_token = "";
-        private static string access_token_secret = "";
+        private readonly static string customer_key = "";
+        private readonly static string customer_key_secret = "";
+        private readonly static string access_token = "";
+        private readonly static string access_token_secret = "";
 
-        private static TwitterService service = new TwitterService(customer_key, customer_key_secret, access_token, access_token_secret);
+        private static int currentImageID = 0;
+        private static readonly TwitterService service = new TwitterService(customer_key, customer_key_secret, access_token, access_token_secret);
+        private static readonly List<string> imageList = new List<string> {""};
 
-        static void Main (string[] args){
+        static void Main(string[] args) {
             Console.WriteLine($"<{DateTime.Now}> - Bot Started");
             SendTweet("TwitterBot says hello world");
             Console.Read();
-
         }
-        private static void SendTweet(string _status){
-            service.SendTweet(new SendTweetOptions { Status = _status}, (tweet, response)) =>{
-                if (response.StatusCode== HttpStatusCode.OK){
+
+        private static void SendTweet(string status) {
+            service.SendTweet(new SendTweetOptions { Status = status }, (tweet, response) => {
+                if (response.StatusCode == HttpStatusCode.OK) {
                     Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine($"<{DateTime.Now}> - Tweet Success!");
-                    Console.ResetColor(); 
-                }
-                else {
+                    Console.ResetColor();
+                } else {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine($"<ERROR>" + response.Error.Message;
-                    Console.ResetColor(); 
+                    Console.WriteLine($"<ERROR> {response.Error.Message}");
+                    Console.ResetColor();
                 }
+            });
+        }
+
+        private static void SendMediaTweet(string status, int imageID) {
+            using (var stream = new FileStream(imageList[imageID], FileMode.Open)) {
+                service.SendTweetWithMedia(new SendTweetWithMediaOptions {
+                    Status = status,
+                    Images = new Dictionary<string, Stream> {{imageList[imageID], stream}}
+                }, (tweet, response) => {
+                    if (response.StatusCode == HttpStatusCode.OK) {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"<{DateTime.Now}> - Tweet Success!");
+                        Console.ResetColor();
+
+                        if (currentImageID + 1 == imageList.Count) {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("<BOT> - End of Image Array");
+                            Console.ResetColor();
+                            currentImageID = 0;
+                        } else {
+                            currentImageID++;
+                        }
+                    } else {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"<ERROR> {response.Error.Message}");
+                        Console.ResetColor();
+                    }
+                });
             }
         }
     }
